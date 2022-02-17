9a1a2c15a23f84fc4dee84dd0785c2c541066953
naman-shrimali <shrimalinaman888@gmail.com>
2022-02-17 13:52:53 +0530

    Enabled All Search Filters

--- src/views/datafilter.vue
+++ src/views/datafilter.vue
@@ -8,7 +8,19 @@
   >
   <h2 align="right" class="banner">Total Number of Entries : {{data_Details.length}}</h2>
   </v-banner>
-  <v-layout row wrap height="5px">
+  
+
+  <v-data-table
+    dense
+    :headers="headers"
+    :items= "data_Details"
+    items-per-page="10"
+    item-key="pat_ID" 
+    :search="search"
+    :custom-filter="filterOnlyCapsText"            
+  >
+    <template v-slot:top>
+        <v-layout row wrap height="5px">
         <!-- 1 -->
         <v-flex xs1>
             
@@ -20,9 +32,9 @@
             outlined
             append-icon="mdi-magnify"
             label="Part CT"
+            v-model="search"
             single-line
             hide-details
-            @input="filterpart_CT"
           ></v-text-field>
       
         </v-flex>
@@ -32,9 +44,9 @@
                 outlined
             append-icon="mdi-magnify"
             label="Dose CT"
+            v-model="dose_CT"
             single-line
             hide-details
-            @input="filterdose_CT"   
             ></v-text-field>
         </v-flex>
         <!-- 4 -->
@@ -44,9 +56,9 @@
                  outlined 
             append-icon="mdi-magnify"
             label="Tumor Type"
+            v-model="tumor"
             single-line
             hide-details
-            @input="filtertumor" 
             ></v-text-field>
         </v-flex>
         <!-- 5 -->
@@ -55,72 +67,76 @@
                    outlined
             append-icon="mdi-magnify"
             label="BOR as"
+            v-model="borAS"
             single-line
             hide-details
-            @input="filterborAS"
             ></v-text-field>
         </v-flex>
         <!-- 6 -->
         <v-flex xs1>
-            <v-select
+            <v-text-field
                color: success  
                outlined  
-            :items="data_Details.orr"
+            :items="authors"
             label="ORR"
+            v-model="orr"
             single-line
             hide-details
-            @change="orr" 
-            ></v-select>
+            ></v-text-field>
         </v-flex>
         <!-- 7 -->
         <v-flex xs1>
-            <v-select
+            <v-text-field
                 outlined  
             label="CB16wks"
+            v-model="cbwks"
             single-line
             hide-details
-            @change="cbwks"  
-            ></v-select>
+            ></v-text-field>
         </v-flex>
         <!-- 8 -->
         <v-flex xs1>
-            <v-select
+            <v-text-field
                color: success
                outlined  
             label="PDL1 IC"
+            v-model="pdlIC"
             single-line
             hide-details
-            @change="pdlIC"    
-            ></v-select>
+            ></v-text-field>
         </v-flex>
         <!-- 9 -->
         <v-flex xs1>
-            <v-select
+            <v-text-field
                 outlined  
             label="HPV Status"
+            v-model="hpvStatus"
             single-line
-            hide-details
-            @change="hpvStatus"     
-            ></v-select>
+            hide-details 
+            ></v-text-field>
         </v-flex>
         <!-- 10 -->
-        <v-flex xs1>
-
-            <v-menu
-                    
-            >
-                <v-text-field
-                     prepend-icon="event"
-                        readonly 
-                        color: success
-                        outlined  
-                ></v-text-field>
-                <v-date-picker
-                        
-                ></v-date-picker>
-
-            </v-menu>
-
+        <v-flex xs1 >
+          <v-col class="pa-0 ma-0">
+            <v-text-field
+               color: success  
+               outlined  
+               label="BOR Data From"
+               v-model="borData_fromDate"
+               single-line
+               hide-details
+            ></v-text-field>
+          </v-col>
+          <v-col class="pa-0 ma-0">
+            <v-text-field
+               color: success  
+               outlined  
+               label="BOR Data To"
+               v-model="borData_toDate"
+               single-line
+               hide-details
+            ></v-text-field>
+          </v-col>
         </v-flex>
         <!-- 11 -->
         <v-flex xs1>
@@ -130,7 +146,8 @@
                     
             >
                 <v-text-field
-                        
+                        label="TS Data"
+                        v-model="tsData"
                 ></v-text-field>
                 <v-date-picker
                         
@@ -141,79 +158,22 @@
         </v-flex>
         <!-- 12 -->
         <v-flex xs1>
-            <v-menu
-                    
-            >
+          
+          <v-menu>
                 <v-text-field
                        color: success
+                       label="CT Date"
+                       v-model="ctDate"
                        outlined 
                 ></v-text-field>
                 <v-date-picker
-                        
                 ></v-date-picker>
-
-            </v-menu>
+          </v-menu>
         </v-flex>
-
-  <v-data-table
-    dense
-    v-scroll.self
-    :headers="headers"
-    :items="data_Details"
-    items-per-page=10
-    item-key="pat_ID"
-    class="elevation-1"
-    :search= "filters"
-    :custom-filter="customFilter"
-                    
-  >
-    <template slot="headers" slot-scope="props">
-                    <tr>
-                        <th>
-                            <v-checkbox
-                                    :input-value="props.all"
-                                    :indeterminate="props.indeterminate"
-                                    primary
-                                    hide-details
-                                    @click.native="toggleAll"
-                            ></v-checkbox>
-                        </th>
-                        <th
-                                v-for="header in props.headers"
-                                :key="header.text"
-                                :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
-                                @click="changeSort(header.value)"
-                        >
-                            <v-icon small>arrow_upward</v-icon>
-                            {{ header.text }}
-                        </th>
-                    </tr>
-                </template>
-    <template slot="data_Details" slot-scope="props">
-                    <tr :active="props.selected" @click="props.selected = !props.selected">
-                        <td>
-                            <v-checkbox
-                                    :input-value="props.selected"
-                                    primary
-                                    hide-details
-                            ></v-checkbox>
-                        </td>
-                        <td>{{ props.item.pat_ID }}</td>
-                        <td >{{ props.item.part_CT}}</td>
-                        <td >{{ props.item.dose_CT }}</td>
-                        <td >{{ props.item.tumor}}</td>
-                        <td >{{ props.item.borAS }}</td>
-                        <td >{{ props.item.orr}}</td>
-                        <td >{{ props.item.cbwks }}</td>
-                        <td >{{ props.item.pdlIC}}</td>
-                        <td >{{ props.item.hpvStatus }}</td>
-                        <td >{{ props.item.borData}}</td>
-                        <td >{{ props.item.tsData }}</td>
-                        <td >{{ props.item.ctDate }}</td>
-                    </tr>
-                </template>
+      </v-layout>
+      </template>
   </v-data-table>
-  </v-layout>
+ 
   </div>
 </template>
 
@@ -222,47 +182,27 @@
 export default {
   name: 'datafilter',
   
-  data() {
-        return{
+  data () {
+    return {
             data_Desc_Url :
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQvQmAr2pxspRHodWbYo6_usmDZ5wXpERkaX35XtFdsgM_b54vzRhUJhp5yQ5bwWGsBJq0lMPVy8Wet/pub?gid=0&single=true&output=csv",
             data_Details:[],
-            //Variables for filters
-            filters: {
-                part_CT: '',
-                dose_CT:'',
-                tumor:'',
-                borAS:'',
-                orr:'',
-                cbwks:'',
-                pdlIC:'',
-                hpvStatus:'',
-                borData: null,
-                tsData: null,
-                ctDate: null,
-            },
-            // part_CT:['A', 'C', 'D', 'E' , 'F' , 'G' , 'H' , 'DA' ],
-            // temp1:'',
-            // dose_CT:'',
-            // tumor:'',
-            // temp3:'',
-            // borAS:'',
-            // temp4:'',
-            // orr:'',
-            // temp5:'',
-            // cbwks:'',
-            // temp6:'',
-            // pdlIC:'',
-            // temp7:'',
-            // hpvStatus:'',
-            // temp8:'',
-            // borData:'',
-            // temp9:'',
-            // tsData:'',
-            // temp10:'',
-            // ctDate:'',
-            // temp11:'',
-            //
+            search: '',
+            part_CT:['A', 'C', 'D', 'E' , 'F' , 'G' , 'H' , 'DA' ],
+            dose_CT:'',
+            tumor:'',
+            borAS:'',
+            orr:'',
+            cbwks:'',
+            pdlIC:'',
+            hpvStatus:'',
+            borData_fromDate:'',
+            borData_toDate:'',
+            menu: false,
+            tsData:'',
+            ctDate:'',
+            
+            authors: ['N', 'NE', 'NA'],
             headers: [
         {
           text: 'Pat ID',
@@ -271,85 +211,79 @@ export default {
           value: 'pat_ID',
         },
         { text: 'Part (CT)', value: 'part_CT'
-        // ,filter: value => {
-        //        if (!this.temp1) return true
-
-        //       return value === this.temp1
-        //     }, 
             },
         { text: 'Dose (CT)', value: 'dose_CT'
-        // ,filter: value => {
-        //      if (!this.dose_CT) return true
+        ,filter: value => {
+             if (!this.dose_CT) return true
 
-        //       return value === this.dose_CT
-        //     }, 
+              return value === this.dose_CT+' mg'
+            }, 
             },
         { text: 'Tumor type Abbrev for INDx 10-14-18', value: 'tumor'
-        // ,filter: value => {
-        //       if (!this.temp3) return true
+        ,filter: value => {
+              if (!this.tumor) return true
 
-        //       return value === this.temp3
-        //     }, 
+              return value === this.tumor.toLocaleUpperCase()
+            }, 
             },
         { text: 'BOR as per GB 060120', value: 'borAS'
-        // ,filter: value => {
-        //       if (!this.temp4) return true
+        ,filter: value => {
+              if (!this.borAS) return true
 
-        //       return value === this.temp4
-        //     }, 
+              return value === this.borAS.toLocaleUpperCase()
+            }, 
             },
         { text: 'ORR 060120', value: 'orr'
-        // ,filter: value => {
-        //       if (!this.calories) return true
+        ,filter: value => {
+              if (!this.orr) return true
 
-        //       return value < parseInt(this.calories)
-        //     }, 
+              return value === this.orr.toLocaleUpperCase()
+            }, 
             },
         { text: 'CB16wks GB060120 (PR,CR, SD16wk)', value: 'cbwks'
-        // ,filter: value => {
-        //       if (!this.calories) return true
+        ,filter: value => {
+              if (!this.cbwks) return true
 
-        //       return value < parseInt(this.calories)
-        //     }, 
+              return value === this.cbwks.toLocaleUpperCase()
+            }, 
             },
         { text: 'PDL1 IC/total nuc >=5% POS', value: 'pdlIC'
-        // ,filter: value => {
-        //       if (!this.calories) return true
+        ,filter: value => {
+              if (!this.pdlIC) return true
 
-        //       return value < parseInt(this.calories)
-        //     }, 
+              return value === this.pdlIC.toLocaleUpperCase()
+            }, 
             },
         { text: 'HPV status', value: 'hpvStatus'
-        // ,filter: value => {
-        //       if (!this.calories) return true
+        ,filter: value => {
+              if (!this.hpvStatus) return true
 
-        //       return value < parseInt(this.calories)
-        //     }, 
+              return value === this.hpvStatus.toLocaleUpperCase()
+            }, 
             },
         { text: 'BOR datacutoff date', value: 'borData'
-        // ,filter: value => {
-        //       if (!this.calories) return true
-
-        //       return value < parseInt(this.calories)
-        //     }, 
+        ,filter: value => {
+              if (!this.borData_fromDate&&!this.borData_toDate) return true
+              return value >= this.borData_fromDate.getTime() && value <= this.borData_toDate.getTime()
+            }, 
             },
         { text: 'TS datacut for CB call', value: 'tsData'
-        // ,filter: value => {
-        //       if (!this.calories) return true
+        ,filter: value => {
+              if (!this.tsData) return true
 
-        //       return value < parseInt(this.calories)
-        //     }, 
+              return value === this.tsData
+            }, 
             },
         { text: 'CT date', value: 'ctDate'
-        // ,filter: value => {
-        //       if (!this.calories) return true
+        ,filter: value => {
+              if (!this.ctDate) return true
 
-        //       return value < parseInt(this.calories)
-        //     }, 
+              return value === this.ctDate
+            }, 
               },
       ],
         }
-    },
+  },
     mounted() {
       window.onresize = function() {
         console.log("changed device dimensions")
@@ -367,76 +301,17 @@ export default {
           }
         });
       },
-      customFilter(items, filters, filter, headers) {
-        // Init the filter class.
-        const cf = new this.$MultiFilters(items, filters, filter, headers);
-        // Use regular function(),
-        // arrow functions does not allow context binding.
-        // Register the global standard filter.
-        cf.registerFilter('search', function (searchWord, items) {
-          if (searchWord.trim() === '') return items;
-          return items.filter(item => {
-            return item.part_CT.toLowerCase().includes(searchWord.toLowerCase());
-          }, searchWord);
-        });
-        // Use regular function(),
-        // arrow functions does not allow context binding.
-        // Register "added_by" filter.
-        cf.registerFilter('orr', function (orr, items) {
-          // If the filter has not been applied yet
-          // just return all available items.
-          if (orr.trim() === '') return items;
-          // Compare each item added_by and just return the matching ones.
-          return items.filter(item => {
-            return item.orr.toLowerCase() === orr.toLowerCase();
-          }, orr);
-        });
-        // Use regular function(),
-        // arrow functions does not allow context binding.
-        // Register "start_date" filter.
-        // cf.registerFilter('start_date', function (start_date, items) {
-        //   // If the filter has not been applied yet
-        //   // just return all available items.
-        //   if (start_date === null) return items;
-        //   // Compare each item start_date and just return the matching ones.
-        //   return items.filter(item => {
-        //     return item.birth_date >= start_date;
-        //   }, start_date);
-        // });
-        // Use regular function(),
-        // arrow functions does not allow context binding.
-        // Register "end_date" filter.
-        // cf.registerFilter('end_date', function (end_date, items) {
-        //   // If the filter has not been applied yet
-        //   // just return all available items.
-        //   if (end_date === null) return items;
-        //   // Compare each item end_date and just return the matching ones.
-        //   return items.filter(item => {
-        //     return item.birth_date <= end_date;
-        //   }, end_date);
-        // });
-        // Its time to run all created filters.
-        // Will be executed in the order thay were defined.
-        return cf.runFilters();
+      // eslint-disable-next-line no-unused-vars
+      filterOnlyCapsText (value, search, item) {
+        return value != null &&
+          search != null &&
+          typeof value === 'string' &&
+          value.toString().toLocaleUpperCase().indexOf(search.toLocaleUpperCase()) !== -1
       },
-      /**
-       * Handler when user input something at the "pat_ID" text field.
-       */
-      filterSearch(val) {
-        this.filters = this.$MultiFilters.updateFilters(this.filters, {part_CT: val});
-      },
-      /**
-       * Handler when user  select some author at the "ORR" select.
-       */
-      filterAuthor(val) {
-        this.filters = this.$MultiFilters.updateFilters(this.filters, {orr: val});
-      },
-
-      // filterOnlyCapsText (value, dose_CT) {
-      //   return value != null &&
-      //     dose_CT != null &&
-      //     typeof value === 'string' &&
-      //     value.toString().toLocaleUpperCase().indexOf(dose_CT) !== -1
+      // filterDate() {
+      //     if (this.borData !== undefined) {
+      //         this.items = this.items.filter((item) => item.borData == this.borData)
+      //     }
       // },
     }
 }
